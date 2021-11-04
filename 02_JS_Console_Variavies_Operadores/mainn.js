7d97c1d0fa0240951608c17fa908228a5ad8cdc9
karolaynecristin <karolaynecristinydarocha@outlook.com>
2021-11-03 21:54:01 -0300

    exercicio

---
+++ 02_JS_Console_Variavies_Operadores/mainn.js
@@ -0,0 +1,13 @@
+// Concatenar strings
+const pessoa = 'karolayne';
+const idade2 = 30;
+console.log('Meu nome é ' + pessoa + ' e eu tenho ' + idade2);
+deixe o  nome  =  'karolayne'
+deixe  peso  =  6,5
+deixe  altura  =  1,60
+deixe  imc  =  peso  /  altura
+
+const  frase  =  nome   +  'tem de altura'  +   altura   +  'e pesa'   +   peso   +  ', seu IMC é'  +  imc
+
+
+console . log ( frase ) ;
---
+++ lição.html/exercicio.js
@@ -0,0 +1,15 @@
+
+
+// push()
+
+const festa2 = ['Joaquim', 'José', 'Silva', 'Xavier'];
+let y = festa2.push('Amanda'); // y = 5
+
+console.log(festa2);
+console.log(y);
+
+const festa1 = ['Joaquim', 'José', 'Silva', 'Xavier', 'Amanda'];
+let x = festa1.pop(); // x = "Xavier"
+
+console.log(festa1);
+console.log(x);
