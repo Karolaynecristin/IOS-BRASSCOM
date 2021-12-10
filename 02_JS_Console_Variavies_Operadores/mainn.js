0546a45945b98bf3ca28fed94d75a0480767d6ab
karolaynecristin <karolaynecristinydarocha@outlook.com>
2021-12-09 21:08:59 -0300

    tarefa java

--- 02_JS_Console_Variavies_Operadores/mainn.js
+++ 02_JS_Console_Variavies_Operadores/mainn.js
@@ -2,10 +2,10 @@
 const pessoa = 'karolayne';
 const idade2 = 30;
 console.log('Meu nome é ' + pessoa + ' e eu tenho ' + idade2);
-deixe o  nome  =  'karolayne'
-deixe  peso  =  6,5
-deixe  altura  =  1,60
-deixe  imc  =  peso  /  altura
+deixe; nome  =  'karolayne'
+deixe; peso  =  6,5
+deixe; altura  =  1,60
+deixe; imc  =  peso  /  altura
 
 const  frase  =  nome   +  'tem de altura'  +   altura   +  'e pesa'   +   peso   +  ', seu IMC é'  +  imc
 
